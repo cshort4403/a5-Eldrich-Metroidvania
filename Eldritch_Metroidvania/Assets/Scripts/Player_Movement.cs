b49700df5edf1ffaeed6dbacabc80ffc3b29a756
cshort4403 <christopher.short1@mohawkcollege.ca>
2023-12-06 17:46:51 -0500

    Add movement and jump
    Added working movement and jump as well as more terrain to test it.

--- Eldritch_Metroidvania/Assets/Scripts/Player_Movement.cs
+++ Eldritch_Metroidvania/Assets/Scripts/Player_Movement.cs
@@ -1,38 +1,106 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-	
-	public float moveSpeed = 1f;
-	public float jumpForce = 1f;
+	private Rigidbody2D rb;
 
-	private bool onGround = false;
-	private Rigidbody2D rb2d;
+	public float MOVE_SPEED = 1f;
+	public float MAX_SPEED = 10f;
+	public float BUTTON_PRESS_TIME = 0.3f;
+	public float FIXED_JUMP_HEIGHT = 1;
+	public float JUMP_CANCEL_RATE = 100;
+	public float JUMP_GRAVITY_SCALE = 1;
+	public float FALL_GRAVITY_SCALE = 10;
+
+	float jumpTime;
+	bool jumping;
+	bool jumpCancelled;
+	bool onGround;
 
 
-
-	void Start()
+	private void Awake()
 	{
-		
-		rb2d = GetComponent<Rigidbody2D>();
+		rb = GetComponent<Rigidbody2D>();
+		rb.freezeRotation = true;
+		rb.gravityScale = FALL_GRAVITY_SCALE;
+		jumping = false;
+		jumpCancelled = false;
+		jumpTime = 0;
+		onGround = false;
+	}
+
+	private void Update()
+	{
+
+		Walk();
+		Jump();
+
 		
 	}
 
-	void Update()
+	private void Walk()
 	{
-
-		rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed,rb2d.velocity.y);
-
-		
-
-		if(onGround && Input.GetButtonDown("Jump"))
+		if(Math.Abs(rb.velocity.x) < MAX_SPEED)
 		{
-			rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+			rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * MOVE_SPEED, 0), ForceMode2D.Impulse);
+		}
+		else
+		{
+			rb.velocity = new Vector2(Input.GetAxis("Horizontal") * MAX_SPEED, rb.velocity.y);
+		}
+		
+		
+	}
+
+	private void Jump()
+	{
+		if (Input.GetButtonDown("Jump") && onGround)
+		{
+			float jumpForce = Mathf.Sqrt(FIXED_JUMP_HEIGHT * -2 * (Physics2D.gravity.y * rb.gravityScale));
+			rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+			rb.gravityScale = JUMP_GRAVITY_SCALE;
+			jumping = true;
+			onGround = false;
+			jumpCancelled = false;
+			jumpTime = 0;
+		}
+		if (jumping)
+		{
+			jumpTime += Time.deltaTime;
+			if (Input.GetButtonUp("Jump"))
+			{
+				jumpCancelled = true;
+				rb.gravityScale = FALL_GRAVITY_SCALE;
+			}
+			if (jumpTime > BUTTON_PRESS_TIME)
+			{
+				jumping = false;
+				rb.gravityScale = FALL_GRAVITY_SCALE;
+			}
+		}
+
+		if(!onGround) 
+		{
+			if(rb.velocity.y == 0)
+			{
+				onGround = true;
+			}
 		}
 
 
 	}
+
+	private void FixedUpdate()
+	{
+		if (jumpCancelled && jumping && rb.velocity.y > 0)
+		{
+			rb.AddForce(Vector2.down * JUMP_CANCEL_RATE);
+		}
+	}
 }
+
 
